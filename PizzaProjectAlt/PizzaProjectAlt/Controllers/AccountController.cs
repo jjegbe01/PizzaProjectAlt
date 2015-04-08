e33b981df8fc91ad74309b94c393645370ddad1c
Julian Egbert <jjegbe01@louisville.edu>
2015-04-08 11:24:23 -0400

    Updated the register method inside of AccountController.cs to reflect the new user attributes

--- PizzaProjectAlt/PizzaProjectAlt/Controllers/AccountController.cs
+++ PizzaProjectAlt/PizzaProjectAlt/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace PizzaProjectAlt.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName, ProfilePicture = model.ProfilePicture, Location = model.Location, FavoritePizzaria = model.FavoritePizzaria, PizzaEatingStyle = model.PizzaEatingStyle};
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
