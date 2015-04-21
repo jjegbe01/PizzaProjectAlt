614723f4f4a38419f7a6c28c15d029b24d757d56
Julian Egbert <jjegbe01@louisville.edu>
2015-04-21 14:28:08 -0400

    Unimportant test migration.
    It was blank.

---
+++ PizzaProjectAlt/PizzaProjectAlt/Migrations/201504211803155_test.cs
@@ -0,0 +1,16 @@
+namespace PizzaProjectAlt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
