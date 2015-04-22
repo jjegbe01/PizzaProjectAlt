0aec581c558ec60584eaa5439bf7abeacde98f5d
maseba01 <maseba01@louisville.edu>
2015-04-22 14:19:52 -0400

    home page styling

--- PizzaProjectAlt/PizzaProjectAlt/App_Start/BundleConfig.cs
+++ PizzaProjectAlt/PizzaProjectAlt/App_Start/BundleConfig.cs
@@ -25,7 +25,13 @@ namespace PizzaProjectAlt
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/carousel.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/homecss")
+            .Include("~/Content/home.css")
+            );
+
         }
     }
 }
