0e9f62d44b1b3bf314687d272b6bae009a3085b2
maseba01 <maseba01@louisville.edu>
2015-04-25 20:03:06 -0400

    added pizza builder

--- PizzaProjectAlt/PizzaProjectAlt/Controllers/HomeController.cs
+++ PizzaProjectAlt/PizzaProjectAlt/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace PizzaProjectAlt.Controllers
             return View();
         }
 
+        public ActionResult PizzaBuilder()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
